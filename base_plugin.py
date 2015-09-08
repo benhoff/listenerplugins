1140291ca7e038c9c331a147752769fc704ed045
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-09-07 21:18:37 -0500

    initial ideas on a plugin auth system

---
+++ base_plugin.py
@@ -0,0 +1,46 @@
+import re
+import types
+from yapsy.IPlugin import IPlugin
+
+def return_error(instance, message, done, *args, **kwargs):
+    if isinstance(done, types.FunctionType):
+        done()
+    done = True
+    # FIXME
+    result = 'You are not authorized to {}'.format(message.command.to_string())
+    return result, done
+
+class ActivityAuth(object):
+    def __init__(self, activity):
+        self.activity = re.compile(activity)
+
+    def __call__(self, f):
+        def wrapped_function(instance, message, done, *args, **kwargs):
+            user = message.user
+            if message.command in instance.matches:
+                if self.activity in self.get_user_activities(user):
+                    return f(instance, message, *args, **kwargs)
+                else:
+                    return return_error(instance, message, done, *args, **kwargs)
+
+    @classmethod
+    def get_user_activities(kls, user):
+        # Required a mapping of roles to activities
+        return [None, False]
+
+class Base(IPlugin):
+    def __init__(self, function=None, config_source=None):
+        super(Base, self).__init__()
+        self.config_source = config_source
+        self.matches = []
+        self.function = function
+    
+    @ActivityAuth('None')
+    def call(self, message, done=None, *args, **kwargs):
+        if command in self.matches:
+            # TODO: add in authentication here
+            result = function(argument, *args, **kwargs)
+            if isinstance(done, types.FunctionType):
+                done()
+            done = True
+            return result, done
